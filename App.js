5cb3d00bf6ff325dfbf4a6f82ba002e6fbf7cb34
codekcg23 <kavigamage23@gmail.com>
2020-06-01 21:20:16 +0530

    20200601-03-logo

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react'; 
 //import ProfileScreen from './screens/profile';
 //import ProfileSettingsScreen from './screens/profileSettings'; 
+import LoginScreen from './screens/Login';
+//import SignupScreen from './screens/Signup';
+import WelcomeScreen from './screens/Welcome';
 
 import AccountSettingsScreen from './screens/accountSettings'; 
 
 
 export default class App extends Component {
   render() {
-    return ( < AccountSettingsScreen /> ) ;
+    return ( < LoginScreen /> ) ;
       
     
   }
 }
  
-    {/* <ProfileSettingsScreen /> //<ProfileScreen />);<Demo /> <Demo /> 
+    {/* <ProfileSettingsScreen /> //<ProfileScreen /> <AccountSettingsScreen/>);<Demo /> <Demo /> 
     //<  */}
