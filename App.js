e1f56d3a80879b3df67d624a212bb19f9dcd0bac
codekcg23 <kavigamage23@gmail.com>
2020-06-02 10:29:58 +0530

    20200602-01-Sign up UI

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 //import ProfileScreen from './screens/profile';
 //import ProfileSettingsScreen from './screens/profileSettings'; 
 import LoginScreen from './screens/Login';
-//import SignupScreen from './screens/Signup';
+import SignupScreen from './screens/Signup';
 import WelcomeScreen from './screens/Welcome';
 
 import AccountSettingsScreen from './screens/accountSettings'; 
@@ -10,7 +10,7 @@ import AccountSettingsScreen from './screens/accountSettings';
 
 export default class App extends Component {
   render() {
-    return ( < LoginScreen /> ) ;
+    return ( < SignupScreen /> ) ;
       
     
   }
--- screens/Signup.js
+++ screens/Signup.js
@@ -1,44 +1,55 @@
 import React,{Component} from 'react';
-import { View,StyleSheet,Text,Button,TouchableOpacity,TextInput } from 'react-native';
+import { View,StyleSheet,Text,Button,TouchableOpacity,TextInput,ScrollView } from 'react-native';
 import BackArrow from '../components/backArrow';
 
-export default class Login extends Component{
+export default class Signup extends Component{
 
     render(){
         return(
             <View style={styles.container}> 
+                <ScrollView>
                 <BackArrow  />
-                <Text style={styles.headerText}>Log in</Text> 
+                <Text style={styles.headerText}>Sign Up</Text> 
                 <View style={styles.card}>
+                    <Text style={ styles.inputTitles }>User Name</Text>
+                    <TextInput
+                        placeholder={'User Name'}
+                        style={styles.textInput}
+                    />
                     <Text style={ styles.inputTitles }>Email</Text>
                     <TextInput
                         placeholder={'Email'}
-                        //  onChangeText={props.handleChange('name')}
-                        //  value={props.values.name}
                         style={styles.textInput}
                     />
                     <Text style={ styles.inputTitles }>Password</Text>
                     <TextInput
                         placeholder={'Password'}
+                        secureTextEntry={true}
+                        style={styles.textInput}
+                    />
+                    <Text style={ styles.inputTitles }>Confirm Password</Text>
+                    <TextInput
+                        placeholder={'Confirm Password'}
+                        secureTextEntry={true}
                         //  onChangeText={props.handleChange('mobile')}
                         //  value={props.values.mobile}
                         style={styles.textInput}
-                        secureTextEntry={true}
                     />
-                 </View>
-                <TouchableOpacity
-                    style={styles.button}
-                    //onPress={this.onPress}
-                >
-                    <Text style={styles.buttonText}> Log in </Text>
-                </TouchableOpacity>
-                <Text 
-                    style={styles.loginText}
-                    //onpress
-                >
-                     Don't have account? Click here to signup
-                </Text> 
-          </View>
+                </View>
+                    <TouchableOpacity
+                        style={styles.button}
+                        //onPress={this.onPress}
+                    >
+                        <Text style={styles.buttonText}> Sign up </Text>
+                    </TouchableOpacity>
+                    <Text 
+                        style={styles.loginText}
+                        //onpress
+                    >
+                        Already have an account ? Click here to Sign in
+                    </Text> 
+                </ScrollView>
+            </View>
         );
 
     }
@@ -50,71 +61,59 @@ const styles = StyleSheet.create({
         fontFamily: 'Segoe UI',
         flex: 1,
         backgroundColor: '#E4E4E4',
-       // backgroundColor:'lightgreen',
 
     },
     headerText:{
         fontSize:30,
         fontFamily: 'Segoe UI',
         fontWeight:'500',
-       // color:'green',
         marginTop:55,
         marginLeft:30,
-       // textAlign:'center',
 
     },
     card:{
        
       //  marginLeft:35,
      //   marginTop:60,
+        fontFamily: 'Segoe UI',
         borderRadius: 6,
         backgroundColor: 'white',
         shadowOpacity: 0.3,
         shadowOffset: {width: 1, height: 1},
        // marginHorizontal: 4,
-        marginLeft: 35,
-        top: 100,
-        height: 250,
+        marginLeft: 40,
+        top: 55,
+        height: 400,
         width: '80%',
         margin: 'auto',
         position: 'relative',
-      //  zIndex: -1,
+        //zIndex: -1,
 
     },
     inputTitles:{
         marginLeft:25,
         fontFamily: 'Segoe UI',
         fontSize:18,
-        marginTop:30,
+        marginTop:25,
         
     },
     textInput:{
-        marginLeft:20,
+        marginLeft:25,
         fontFamily: 'Segoe UI',
         fontSize:15,
       //  marginTop:0,
-       // marginLeft:20,
-      //  marginRight:30,
         color: '#404040',
         width: '80%',
-      //  alignSelf: "center",
         borderColor: "#ccc",
         borderBottomWidth: 1,
-      //  borderWidth:1,
-      //  borderRightWidth:10,
-        
-      //  backgroundColor:'white',
-
     },
-   
     button:{
         backgroundColor:'green',
-        marginTop:200,
-        padding:10,
+        marginTop:95,
+        padding:9,
         alignItems:'center',
         marginLeft:100,
         marginRight:100,
-
     },
     buttonText:{
         fontFamily: 'Segoe UI',
@@ -125,7 +124,7 @@ const styles = StyleSheet.create({
     loginText:{
         fontFamily: 'Segoe UI',
         color:'green',
-        marginLeft:70,
+        marginLeft:55,
         marginTop:10
 
     }
